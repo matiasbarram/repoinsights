7a5c95d71692ff52fe8b245d35ce7339dbeb1fdd
Matias Barra <matias.barra.ap@copec.cl>
2023-04-04 12:08:09 -0400

    rm not used method of GHRepository

--- GithubExtractor/GHRepository.py
+++ GithubExtractor/GHRepository.py
@@ -1,16 +1,8 @@
 from .GHGetToken import GHGetToken
-from github import Github, Repository, Issue, Label
+from github import Repository
 
 
 class GHRepository:
     def __init__(self, gh_token: GHGetToken, gh_repo: str) -> None:
         self.connector = gh_token.connector
         self.repo: Repository.Repository = self.connector.get_repo(gh_repo)
-
-    def get_project_data(self, label: Label.Label):
-        data = {
-            "repo_id": self.repo.id,
-            "name": label.name,
-        }
-        print(f"label data {data}")
-        return data
