aeee590ba0033fb7a3f1c6fe2e7d99afb65d5d5d
Matias Barra <matias.barra@alumnos.uach.cl>
2023-06-14 16:23:44 -0400

    rename pendientes_service to pending_service

--- pending.py
+++ pending.py
@@ -6,63 +6,16 @@ import pika
 import argparse
 from services.pendientes_service.connector import DBController
 from services.pendientes_service.database_handler import PendingProjectsController
-
-
-class RabbitMQError(Exception):
-    """
-    Handle all rabbitmq errors
-    """
-
-
-class QueueController:
-    """
-    Class to connect to rabbitmq
-    """
-
-    def __init__(self) -> None:
-        self.rabbit_user = os.environ["RABBIT_USER"]
-        self.rabbit_pass = os.environ["RABBIT_PASS"]
-        self.rabbit_host = os.environ["RABBIT_HOST"]
-        self.channel = None
-
-    def connect(self):
-        """
-        Create or get a channel to rabbitmq
-        """
-        credentials = pika.PlainCredentials(self.rabbit_user, self.rabbit_pass)
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=self.rabbit_host, credentials=credentials)
-        )
-        self.channel = connection.channel()
-        self.channel.queue_declare(queue="pendientes", durable=True)
-
-    def enqueue(self, project: Dict[str, Any]):
-        """
-        Add message to queue pendientes
-        """
-        project_json = json.dumps(project, default=str)
-        if self.channel is None:
-            raise RabbitMQError("Channel not found")
-
-        self.channel.basic_publish(
-            exchange="",
-            routing_key="pendientes",
-            body=project_json,
-            properties=pika.BasicProperties(
-                delivery_mode=2,
-            ),
-            mandatory=True,
-        )
-        logger.info("Enqueued project {project}", project=project_json)
+from services.pendientes_service.queue_controller import QueueController
 
 
 def main(debug: bool):
     """
-    Almacena en cola todos los proyectos que no han sido actualizados.
+    Main function
     """
     logger.info("Starting pendientes service DEBUG={debug}", debug=debug)
-    connector = DBController()
-    pending_projects = PendingProjectsController(connector)
+    db_connector = DBController()
+    pending_projects = PendingProjectsController(db_connector)
     queue_client = QueueController()
     queue_client.connect()
     projects = (
