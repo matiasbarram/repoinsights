e3d4a973bcd00efc5387ed3f5d3b03df8968288e
Matias Barra <matias.barra@alumnos.uach.cl>
2023-06-22 20:18:19 -0400

    calc all projects metrics

--- metrics.py
+++ metrics.py
@@ -10,22 +10,22 @@ def save_results_to_file(results):
         f.write(json_dump)
 
 
-def main(project_id, extraction_id):
+def main():
     consolidada_conn = ConsolidadaConnection()
     conn = consolidada_conn.get_connection()
-    # projects = consolidada_conn.get_all_projects()
-    # for project in projects:
-    #     extraction_id, project_id = project
-    calc_metrics = CalculateMetrics(conn, project_id, extraction_id)
-    load_metrics = MetricsLoader(conn, project_id, extraction_id)
-    results = calc_metrics.calculate_metrics()
+    projects = consolidada_conn.get_all_projects()
+    for project in projects:
+        extraction_id, project_id = project
+        calc_metrics = CalculateMetrics(conn, project_id, extraction_id)
+        load_metrics = MetricsLoader(conn, project_id, extraction_id)
+        results = calc_metrics.calculate_metrics()
 
-    save_results_to_file(results)
+        save_results_to_file(results)
 
-    for metric_group in calc_metrics.metrics:
-        load_metrics.add_metric_group(metric_group)
-    load_metrics.load_metrics(results)
+        for metric_group in calc_metrics.metrics:
+            load_metrics.add_metric_group(metric_group)
+        load_metrics.load_metrics(results)
 
 
 if __name__ == "__main__":
-    main(1, 1)
+    main()
